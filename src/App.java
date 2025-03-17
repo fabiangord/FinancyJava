c15bc0901e5d53b00a290ef6da285021cc8d10d3
James <jamesalejandromelendez@hotmail.com>
2025-03-16 23:16:00 -0500

    Creacion vista Saving

--- src/App.java
+++ src/App.java
@@ -1,20 +1,23 @@
 import java.math.BigInteger;
-
+import View.MainFrame;
 import Controller.BudgetController;
 import Controller.IncomeController;
 import Controller.SavingController;
 public class App {    public static void main(String[] args) throws Exception {
     System.out.println("Hello, World!");
 
+    MainFrame ventana = new MainFrame();
+    ventana.initialize();
+
     //Saving
-    SavingController controllerSaving = new SavingController();
+    //SavingController controllerSaving = new SavingController();
     //controllerSaving.addSaving("Juan Pepito", "TRANSPORTE", BigInteger.valueOf(10000));
     //controllerSaving.updateSaving("Juan perez", BigInteger.valueOf(10000), "TRANSPORTE", 4);
     //controllerSaving.deleteSaving(4);
     //controllerSaving.getAllSaving();
-    controllerSaving.getOneSaving(1);
+    //controllerSaving.getOneSaving(1);
     
-    BudgetController controllerBudget = new BudgetController();
+    //BudgetController controllerBudget = new BudgetController();
     //controllerBudget.addBudget("Juan Presupuestos", "TRANSPORTE", BigInteger.valueOf(10000));
     //controllerBudget.getAllBudget();
     
